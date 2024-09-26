e4ac634f764fcd83c85415a127e0ae6c10f77b5b
Kamal <kamal.farahani90@gmail.com>
2024-09-26 15:23:28 +0330

    add PlayerState

---
+++ zombie_knight/state/player.py
@@ -0,0 +1,28 @@
+import pygame
+
+from dataclasses import dataclass
+from enum import Enum
+
+from .matter import Matter
+
+
+class PlayerMode(Enum):
+    idle = 0
+    walking = 1
+    jumping = 2
+
+
+@dataclass
+class PlayerState(Matter):
+    rect: pygame.Rect
+    mode: PlayerMode
+    lives: int
+    score: int
+    velocity: pygame.Vector2
+    acceleration: pygame.Vector2
+
+    def get_velocity(self) -> pygame.Vector2:
+        return self.velocity
+
+    def get_acceleration(self) -> pygame.Vector2:
+        return self.acceleration
