07237ec631a341014ad4b2cadf040b2c6e297bc9
Kamal <kamal.farahani90@gmail.com>
2024-09-26 19:48:44 +0330

    add images paths

---
+++ zombie_knight/constants/paths.py
@@ -0,0 +1,6 @@
+DIRT_TILE_IMAGE_PATH = "./assets/images/tiles/Tile (1).png"
+GRASS_TILE_IMAGE_PATH = "./assets/images/tiles/Tile (2).png"
+LEFT_TILE_IMAGE_PATH = "./assets/images/tiles/Tile (3).png"
+RIGHT_TILE_IMAGE_PATH = "./assets/images/tiles/Tile (5).png"
+
+BACKGROUND_IMAGE_PATH = "./assets/images/background.png"
