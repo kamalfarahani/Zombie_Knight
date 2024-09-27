7ce48e99d7bce0e8b6f93096ab7deed5e9b09387
Kamal <kamal.farahani90@gmail.com>
2024-09-27 14:51:02 +0330

    add knight image paths

--- zombie_knight/constants/paths.py
+++ zombie_knight/constants/paths.py
@@ -4,3 +4,56 @@ LEFT_TILE_IMAGE_PATH = "./assets/images/tiles/Tile (3).png"
 RIGHT_TILE_IMAGE_PATH = "./assets/images/tiles/Tile (5).png"
 
 BACKGROUND_IMAGE_PATH = "./assets/images/background.png"
+
+
+PLAYER_ATTACK_IMAGE_PATHS = [
+    "./assets/images/player/attack/Attack (1).png",
+    "./assets/images/player/attack/Attack (2).png",
+    "./assets/images/player/attack/Attack (3).png",
+    "./assets/images/player/attack/Attack (4).png",
+    "./assets/images/player/attack/Attack (5).png",
+    "./assets/images/player/attack/Attack (6).png",
+    "./assets/images/player/attack/Attack (7).png",
+    "./assets/images/player/attack/Attack (8).png",
+    "./assets/images/player/attack/Attack (9).png",
+    "./assets/images/player/attack/Attack (10).png",
+]
+
+PLAYER_IDLE_IMAGE_PATHS = [
+    "./assets/images/player/idle/Idle (1).png",
+    "./assets/images/player/idle/Idle (2).png",
+    "./assets/images/player/idle/Idle (3).png",
+    "./assets/images/player/idle/Idle (4).png",
+    "./assets/images/player/idle/Idle (5).png",
+    "./assets/images/player/idle/Idle (6).png",
+    "./assets/images/player/idle/Idle (7).png",
+    "./assets/images/player/idle/Idle (8).png",
+    "./assets/images/player/idle/Idle (9).png",
+    "./assets/images/player/idle/Idle (10).png",
+]
+
+PLAYER_RUN_IMAGE_PATHS = [
+    "./assets/images/player/run/Run (1).png",
+    "./assets/images/player/run/Run (2).png",
+    "./assets/images/player/run/Run (3).png",
+    "./assets/images/player/run/Run (4).png",
+    "./assets/images/player/run/Run (5).png",
+    "./assets/images/player/run/Run (6).png",
+    "./assets/images/player/run/Run (7).png",
+    "./assets/images/player/run/Run (8).png",
+    "./assets/images/player/run/Run (9).png",
+    "./assets/images/player/run/Run (10).png",
+]
+
+PLAYER_JUMP_IMAGE_PATHS = [
+    "./assets/images/player/jump/Jump (1).png",
+    "./assets/images/player/jump/Jump (2).png",
+    "./assets/images/player/jump/Jump (3).png",
+    "./assets/images/player/jump/Jump (4).png",
+    "./assets/images/player/jump/Jump (5).png",
+    "./assets/images/player/jump/Jump (6).png",
+    "./assets/images/player/jump/Jump (7).png",
+    "./assets/images/player/jump/Jump (8).png",
+    "./assets/images/player/jump/Jump (9).png",
+    "./assets/images/player/jump/Jump (10).png",
+]
