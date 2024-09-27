336e98111408bbf1957378928c3467f447e664ce
Kamal <kamal.farahani90@gmail.com>
2024-09-27 16:51:37 +0330

    Implement get_matters() method

--- zombie_knight/state/game_state.py
+++ zombie_knight/state/game_state.py
@@ -2,9 +2,17 @@ from dataclasses import dataclass
 
 from .player import PlayerState
 from .tile import TileState
+from .matter import Matter
 
 
 @dataclass
 class GameState:
     player: PlayerState
     tiles: list[TileState]
+
+    def get_matters(self) -> list[Matter]:
+        return [
+            getattr(self, obj)
+            for obj in dir(self)
+            if isinstance(getattr(self, obj), Matter)
+        ]
