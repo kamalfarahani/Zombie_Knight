f4c422198c1da6ed80b5d1c68fbc2654ab2cd0bb
Kamal <kamal.farahani90@gmail.com>
2024-09-27 16:45:19 +0330

    Implement Matter methods

--- zombie_knight/state/player.py
+++ zombie_knight/state/player.py
@@ -1,6 +1,8 @@
+from __future__ import annotations
+
 import pygame
 
-from dataclasses import dataclass
+from dataclasses import dataclass, replace
 from enum import Enum
 
 from .matter import Matter
@@ -25,8 +27,39 @@ class PlayerState(Matter):
     velocity: pygame.Vector2
     acceleration: pygame.Vector2
 
+    def get_position(self) -> pygame.Vector2:
+        position = pygame.Vector2(
+            self.rect.x,
+            self.rect.y,
+        )
+
+        return position
+
     def get_velocity(self) -> pygame.Vector2:
         return self.velocity
 
     def get_acceleration(self) -> pygame.Vector2:
         return self.acceleration
+
+    def replace_position(self, position: pygame.Vector2) -> PlayerState:
+        return replace(
+            self,
+            rect=pygame.Rect(
+                position.x,
+                position.y,
+                self.rect.width,
+                self.rect.height,
+            ),
+        )
+
+    def replace_velocity(self, velocity: pygame.Vector2) -> PlayerState:
+        return replace(
+            self,
+            velocity=velocity,
+        )
+
+    def replace_acceleration(self, acceleration: pygame.Vector2) -> PlayerState:
+        return replace(
+            self,
+            acceleration=acceleration,
+        )
