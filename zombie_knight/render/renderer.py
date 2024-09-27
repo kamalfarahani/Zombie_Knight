6c832b8a6f10566315fe0f111f081b32383ab8ff
Kamal <kamal.farahani90@gmail.com>
2024-09-27 14:51:46 +0330

    add `render_player()` method

--- zombie_knight/render/renderer.py
+++ zombie_knight/render/renderer.py
@@ -1,8 +1,10 @@
 import pygame
 
 from .tile import create_tile_sprites
+from .player import Player
 from ..state.game_state import GameState
 from ..state.tile import TileState
+from ..state.player import PlayerState
 from ..constants.paths import BACKGROUND_IMAGE_PATH
 
 
@@ -23,6 +25,7 @@ class Renderer:
     ) -> None:
         self.render_background()
         self.render_tiles(game_state.tiles)
+        self.render_player(game_state.player)
 
     def render_background(self) -> None:
         self.display.blit(self.background_image, (0, 0))
@@ -34,3 +37,7 @@ class Renderer:
         tile_sprites = create_tile_sprites(tile_states)
         for tile in tile_sprites:
             self.display.blit(tile.image, tile.rect)
+
+    def render_player(self, player_state: PlayerState) -> None:
+        player = Player(player_state)
+        self.display.blit(player.image, player.rect)
